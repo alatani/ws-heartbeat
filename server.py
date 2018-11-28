8521495a2050a4320f9c5afaf481d43e3801f6ea
atsuhiro-narita <alayashiki@gmail.com>
2018-11-29 01:33:01 +0900

    --wip--

--- server.py
+++ server.py
@@ -1,22 +1,29 @@
 #encode: utf-8
+import time
 from websocket_server import WebsocketServer
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
-	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
+    print("New client connected and was given id %d" % client['id'])
+    server.send_message_to_all("Hey all, a new client has joined us")
 
 
 # Called for every client disconnecting
 def client_left(client, server):
-	print("Client(%d) disconnected" % client['id'])
+    print("Client(%d) disconnected" % client['id'])
 
 
 # Called when a client sends a message
 def message_received(client, server, message):
-	if len(message) > 200:
-		message = message[:200]+'..'
-	print("Client(%d) said: %s" % (client['id'], message))
+    if len(message) > 200:
+        message = message[:200]+'..'
+    print("Client(%d) said: %s" % (client['id'], message))
+
+    count = 0
+    while True:
+        server.send_message_to_all("ping:" + str(count))
+        count += 1
+        time.sleep(0.2)
 
 if __name__ == "__main__":
     PORT=4001
@@ -24,5 +31,6 @@ if __name__ == "__main__":
     server.set_fn_new_client(new_client)
     server.set_fn_client_left(client_left)
     server.set_fn_message_received(message_received)
+
     server.run_forever()
 
